2c7412319d288cf00ef6a1c199784b975646039e
Tomas 'Jethro' Pokorny <xtompok@gmail.com>
2021-11-12 00:23:15 +0100

    Improvements, better control of sending.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# radio-range-tester
+# Radio range tester
 Scripts for testing range of radio transcievers
+
+## Process
+ - packet is sent once per second
+ - mobile unit sends packets between 20th and 60th second
+ - stabile unit sends packets between 0th and 40th second
+ - time of send within the second is set by config choice `send_shift_ms`
--- stabile.py
+++ stabile.py
@@ -15,57 +15,73 @@ from signal import SIGINT, SIGTERM
 # First 32 bytes escaped with 0x7D + <byte> ^ 0x20
 
 class RadioProtocol(asyncio.Protocol):
-    def __init__(self, config, csvwriter):
-        self.csvwriter = csvwriter
-        self.csvwriter.writerow(packet_fieldnames())
-        self.recv_packet_id = None
+	def __init__(self, config, csvwriter):
+		self.csvwriter = csvwriter
+		self.csvwriter.writerow(packet_fieldnames())
+		self.recv_packet_id = None
+		self.config = config
 
 
-    def connection_made(self,transport):
-        self.transport = transport
-        self.buf = []
-        asyncio.ensure_future(self.send())
-        print("Radio connected")
-    
-    def connection_lost(self,exc):
-        self.csvfile.close()
-        print("Radio disconnected")
+	def connection_made(self,transport):
+		self.transport = transport
+		self.buf = []
+		asyncio.ensure_future(self.send())
+		print("Radio connected")
+	
+	def connection_lost(self,exc):
+		self.csvfile.close()
+		print("Radio disconnected")
 
-    def data_received(self,data):
-        #print(f"Got data: {data}")
-        if len(self.buf) > 30:
-            self.buf.clear()
-        for b in data:
-            if b != 0x02 and len(self.buf) == 0:
-                print("Waiting for 0x02...")
-                continue
+	def data_received(self,data):
+		#print(f"Got data: {data}")
+		if len(self.buf) > 30:
+			self.buf.clear()
+		for b in data:
+			if b != 0x02 and len(self.buf) == 0:
+				print("Waiting for 0x02...")
+				continue
 
-            self.buf.append(b)
-            if b != 0x03:
-                continue
-            
-            #print(f"Read:{self.buf}")
-            data = decode_packet(self.buf)
-            self.buf.clear()
-            if data is None:
-                continue    
-            if self.recv_packet_id is not None and data[0] != self.recv_packet_id+1:
-                print(f"Lost packet(s): last received: {self.recv_packet_id}, now received: {data[0]}")
-            self.recv_packet_id = data[0]
-            print(data)
-            self.csvwriter.writerow(data)
+			self.buf.append(b)
+			if b != 0x03:
+				continue
+			
+			#print(f"Read:{self.buf}")
+			data = decode_packet(self.buf)
+			now = datetime.now()
+			self.buf.clear()
+			if data is None:
+				continue	
+			if self.recv_packet_id is not None and data[0] != self.recv_packet_id+1:
+				print(f"Lost packet(s): last received: {self.recv_packet_id}, now received: {data[0]}")
+			self.recv_packet_id = data[0]
+			print(f"{now.second}.{now.microsecond}: {data}")
+			self.csvwriter.writerow(data)
 
-    async def send(self):
-        pkt_id = 0
+	async def send(self):
+		pkt_id = 0
+		
+		last_second_sent = datetime.now().second
+		while True:
+			atime = int(time.time())
+			now = datetime.now()
+			if now.second >= 40:
+				await asyncio.sleep(1)
+				continue
+			
+			if last_second_sent == now.second:
+				await asyncio.sleep(0.001)
+				continue
+			if now.microsecond/1000 < self.config['radio']['send_shift_ms']:
+				await asyncio.sleep(0.001)
+				continue
 
-        while True:
-            atime = int(time.time())
-            buf = encode_packet(pkt_id, atime, 0, 0)
-            #print(buf)
-            self.transport.serial.write(buf)
-            print(f"Packet {pkt_id} sent")
-            pkt_id = (pkt_id+1)%65536
-            await asyncio.sleep(1)
+			buf = encode_packet(pkt_id, atime, 0, 0)
+			#print(buf)
+			self.transport.serial.write(buf)
+			last_second_sent = now.second
+			print(f"{now.second}.{now.microsecond}: Packet {pkt_id} sent")
+			pkt_id = (pkt_id+1)%65536
+			await asyncio.sleep(0.1)
 
 
 
@@ -73,23 +89,23 @@ class RadioProtocol(asyncio.Protocol):
 @click.command()
 @click.argument('config_file',type=click.File('r'))
 def run(config_file):
-    config = toml.load(config_file)
-#    radio = serial.Serial(config['radio']['port'],baudrate=config['radio']['baudrate'], timeout=1)
-    csvname = config['log']['csv_prefix']+"_"+datetime.strftime(datetime.now(),"%Y-%m-%d_%H-%M-%S")+".csv"
-    csvpath = Path(config['log']['directory'])/Path(csvname)
+	config = toml.load(config_file)
+#	radio = serial.Serial(config['radio']['port'],baudrate=config['radio']['baudrate'], timeout=1)
+	csvname = config['log']['csv_prefix']+"_"+datetime.strftime(datetime.now(),"%Y-%m-%d_%H-%M-%S")+".csv"
+	csvpath = Path(config['log']['directory'])/Path(csvname)
 
-    with open(csvpath,"w") as csvfile:
-        csvwriter = csv.writer(csvfile)
+	with open(csvpath,"w") as csvfile:
+		csvwriter = csv.writer(csvfile)
 
-        loop = asyncio.get_event_loop()
-        radio = serial_asyncio.create_serial_connection(loop, lambda: RadioProtocol(config,csvwriter), config['radio']['port'], baudrate = config['radio']['baudrate'])
-        task = asyncio.ensure_future(radio)
-        print("Running loop")
-        loop.run_forever()
-    print("Loop ended")
-    exit()
+		loop = asyncio.get_event_loop()
+		radio = serial_asyncio.create_serial_connection(loop, lambda: RadioProtocol(config,csvwriter), config['radio']['port'], baudrate = config['radio']['baudrate'])
+		task = asyncio.ensure_future(radio)
+		print("Running loop")
+		loop.run_forever()
+	print("Loop ended")
+	exit()
 
 
 if __name__ == "__main__":
-    run()
+	run()
 
